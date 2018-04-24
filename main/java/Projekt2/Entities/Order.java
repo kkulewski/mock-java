e7a71ab9e727e275773bab703330255d04a966f4
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-24 23:04:06 +0200

    Add composite order, orderItem POJO

---
+++ main/java/Projekt2/Entities/Order.java
@@ -0,0 +1,27 @@
+package Projekt2.Entities;
+
+public class Order
+{
+    private int id;
+    private int clientId;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getClientId()
+    {
+        return clientId;
+    }
+
+    public void setClientId(int clientId)
+    {
+        this.clientId = clientId;
+    }
+}
---
+++ main/java/Projekt2/Entities/OrderItem.java
@@ -0,0 +1,27 @@
+package Projekt2.Entities;
+
+public class OrderItem
+{
+    private int orderId;
+    private int itemId;
+
+    public int getOrderId()
+    {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId)
+    {
+        this.orderId = orderId;
+    }
+
+    public int getItemId()
+    {
+        return itemId;
+    }
+
+    public void setItemId(int itemId)
+    {
+        this.itemId = itemId;
+    }
+}
