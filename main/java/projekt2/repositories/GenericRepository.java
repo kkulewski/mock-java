b41f658027a345f27d9f46e052d3890e0dcd87c8
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-25 19:07:51 +0200

    Simplify generic repo

--- main/java/projekt2/repositories/GenericRepository.java
+++ main/java/projekt2/repositories/GenericRepository.java
@@ -2,7 +2,7 @@ package projekt2.repositories;
 
 import java.util.List;
 
-public interface GenericRepository<TEntity extends Object>
+public interface GenericRepository<TEntity>
 {
     TEntity getById(int id);
     List<TEntity> getAll();
