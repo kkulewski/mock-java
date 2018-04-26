44f8f43d553fe5a6cd6fc82ec8593c6f8e5f020e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-27 00:52:10 +0200

    Add order item add in service

--- main/java/projekt2/services/OrderService.java
+++ main/java/projekt2/services/OrderService.java
@@ -1,7 +1,10 @@
 package projekt2.services;
 
 import projekt2.entities.Client;
+import projekt2.entities.Item;
 import projekt2.entities.Order;
+import projekt2.entities.OrderItem;
+import projekt2.repositories.OrderItemRepository;
 import projekt2.repositories.OrderRepository;
 
 import java.util.List;
@@ -9,14 +12,27 @@ import java.util.List;
 public class OrderService
 {
     private OrderRepository orderRepo;
+    private OrderItemRepository orderItemRepo;
 
-    public OrderService(OrderRepository orderRepo)
+    public OrderService(OrderRepository orderRepo, OrderItemRepository orderItemRepo)
     {
         this.orderRepo = orderRepo;
+        this.orderItemRepo = orderItemRepo;
     }
 
     public List<Order> getClientOrders(Client client)
     {
         return orderRepo.getByClientId(client.getId());
+    }
+
+    public boolean addItemToOrder(Item item, Order order)
+    {
+        if (orderItemRepo.getByItemId(item.getId()) == null)
+        {
+            OrderItem orderItem = new OrderItem(order.getId(), item.getId());
+            return orderItemRepo.add(orderItem);
+        }
+
+        return false;
     }
 }
