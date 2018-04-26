a550a38b109410d824fd5077b1af339a13463665
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-27 00:42:45 +0200

    Add get client orders test

---
+++ test/projekt2/services/OrderServiceMockitoTest.java
@@ -0,0 +1,46 @@
+package projekt2.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import projekt2.entities.Client;
+import projekt2.entities.Order;
+import projekt2.extensions.MockitoExtension;
+import projekt2.repositories.OrderRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceMockitoTest
+{
+    private OrderRepository orderRepo;
+
+    private OrderService os;
+
+    @BeforeEach
+    void setup()
+    {
+        orderRepo = Mockito.mock(OrderRepository.class);
+        os = new OrderService(orderRepo);
+    }
+
+    @Test
+    void getClientOrdersReturnsExpectedOrders()
+    {
+        // Arrange
+        Client client = new Client(1, "John", "Doe", "johndoe@email.com");
+        List<Order> expectedOrders = Arrays.asList(new Order(1, client.getId()), new Order(2, client.getId()));
+        doReturn(expectedOrders).when(orderRepo).getByClientId(client.getId());
+
+        // Act
+        List<Order> result = os.getClientOrders(client);
+
+        // Assert
+        assertThat(result).containsAll(expectedOrders);
+    }
+}
