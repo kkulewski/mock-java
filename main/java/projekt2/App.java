c5214b8c67bf12716ce1e3ee66f7d0a12b7130ad
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-27 03:02:49 +0200

    Update demo

--- main/java/projekt2/App.java
+++ main/java/projekt2/App.java
@@ -9,19 +9,8 @@ public class App
     public static void main(String[] args)
     {
         ClientRepository cr = new InMemoryClientRepository();
-
-        Client john = new Client();
-        john.setId(1);
-        john.setEmail("john@email.com");
-
-        Client dave = new Client();
-        dave.setId(2);
-        dave.setEmail("dave@email.com");
-
-
-        cr.add(john);
-        cr.add(dave);
-
+        cr.add(new Client(1, "John", "Doe", "john@email.com"));
+        cr.add(new Client(2, "Dave", "Smith", "dave@email.com"));
         Client result = cr.getById(1);
         Client result2 = cr.getById(3);
         System.out.println(result.getEmail());
