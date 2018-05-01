2b288c53d446feae71756522223df55dd91b2dab
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-02 00:57:55 +0200

    Move in-memory client repo to mocks

--- main/java/projekt2/App.java
+++ main/java/projekt2/App.java
@@ -1,18 +1,16 @@
 package projekt2;
 
-import projekt2.entities.*;
-import projekt2.repositories.*;
-import projekt2.repositories.inMemory.*;
-
 public class App
 {
     public static void main(String[] args)
     {
+        /*
         ClientRepository cr = new InMemoryClientRepository();
         cr.add(new Client(1, "John", "Doe", "john@email.com"));
         cr.add(new Client(2, "Dave", "Smith", "dave@email.com"));
         Client result = cr.getById(1);
         Client result2 = cr.getById(3);
         System.out.println(result.getEmail());
+        */
     }
 }
--- test/projekt2/mocks/InMemoryClientRepository.java
+++ test/projekt2/mocks/InMemoryClientRepository.java
@@ -1,7 +1,8 @@
-package projekt2.repositories.inMemory;
+package projekt2.mocks;
 
 import projekt2.entities.Client;
 import projekt2.repositories.ClientRepository;
+import projekt2.repositories.inMemory.InMemoryGenericRepository;
 
 public class InMemoryClientRepository extends InMemoryGenericRepository<Client> implements ClientRepository
 {
