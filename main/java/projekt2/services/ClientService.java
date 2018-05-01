f99b173d7e2da52eeac2797a4f258f533010d882
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-02 01:03:10 +0200

    Add client service

---
+++ main/java/projekt2/services/ClientService.java
@@ -0,0 +1,27 @@
+package projekt2.services;
+
+import projekt2.entities.Client;
+import projekt2.repositories.ClientRepository;
+import projekt2.validators.ClientValidator;
+
+public class ClientService
+{
+    private ClientValidator clientValidator;
+    private ClientRepository clientRepo;
+
+    public ClientService(ClientValidator clientValidator, ClientRepository clientRepo)
+    {
+        this.clientValidator = clientValidator;
+        this.clientRepo = clientRepo;
+    }
+
+    public boolean addClient(Client client)
+    {
+        if (!clientValidator.isValid(client))
+        {
+            return false;
+        }
+
+        return clientRepo.add(client);
+    }
+}
