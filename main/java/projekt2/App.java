b7cdcd1db8d47368660e9fe50f924cc71de77b73
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-02 02:28:02 +0200

    Remove demo code

--- main/java/projekt2/App.java
+++ main/java/projekt2/App.java
@@ -4,13 +4,5 @@ public class App
 {
     public static void main(String[] args)
     {
-        /*
-        ClientRepository cr = new InMemoryClientRepository();
-        cr.add(new Client(1, "John", "Doe", "john@email.com"));
-        cr.add(new Client(2, "Dave", "Smith", "dave@email.com"));
-        Client result = cr.getById(1);
-        Client result2 = cr.getById(3);
-        System.out.println(result.getEmail());
-        */
     }
 }
